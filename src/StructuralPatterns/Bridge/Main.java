d1793c114abb3f6c4db96f35a702d079143052e5
Saurabh Chinchamalatpure <saurabh.chinchamalatpure@plus91.in>
2024-02-08 14:59:16 +0530

    Bridge Design Pattern

--- src/StructuralPatterns/Bridge/Main.java
+++ src/StructuralPatterns/Bridge/Main.java
@@ -2,6 +2,19 @@ package StructuralPatterns.Bridge;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+            "Device" is the implementor interface, and "Radio" and "TV" are its concrete implementations.
+            They provide specific implementations for device operations.
+
+            "RemoteControl" is the abstraction, and "BasicRemoteControl" is a refined abstraction that adds some more functionality.
+            It uses a "Device" object to perform operations.
+
+            The "Device" can change independently of the "RemoteControl", and vice versa.
+            This is the "BRIDGE" between the two different hierarchies — the "RemoteControl" and the "Devices" it controls.
+
+            In the below code, we demonstrate using the "RemoteControl" with both a "Radio" and a "TV" without altering the "RemoteControl" code.
+         */
+
         //! TV device
         Device tv = new TV();
         RemoteControl tvRemoteControl = new BasicRemoteControl(tv);
